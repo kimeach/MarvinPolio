f0d4f50249c8b763bae020925f341cf601aafdbd
gmKim <gmKim@DESKTOP-GA7G7OV>
2020-05-05 23:53:30 +0900

    slide 기능 추가 및 index 화면 수정

--- Intro/target/classes/static/js/marvin.1.0.0.js
+++ Intro/target/classes/static/js/marvin.1.0.0.js
@@ -1,4 +1,25 @@
 /**
- * 2020년 04월 29일 최초 수정
+ * 2020년 04월 29일 최초 생성
  */
 
+			$(function(){
+				$('#sidebarBtnCover').click(function(e){
+					et = e.target;
+					$('#sidebarBtn').html('<i class="fa fa-angle-double-left fa-lg" aria-hidden="true"></i>')
+					$('.iconName').css("display","block");
+					
+					if(parseInt($('#sidebar').css("width")) === parseInt(innerWidth*0.04)){
+						$('#sidebar').animate({
+							width : '7%'
+						},
+						200);
+					}else{
+						$('.iconName').css("display","none");
+						$('#sidebar').animate({
+							width : '4%'
+						},
+						200);
+						$('#sidebarBtn').html('<i class="fa fa-angle-double-right fa-lg" aria-hidden="true"></i>')
+					}
+				})
+			});	
